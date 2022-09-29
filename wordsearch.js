1815347df90037bb92d81560568a22d89b9ec4d6
Anit <anittankandasamy@gmail.com>
2022-09-29 05:07:33 +0000

    created a function that will transpose given aray to find word meaning its searching the original array vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(letters) {
+  const newArray = [];
+  for (let col = 0; col < letters[0].length; col++) {
+    newArray.push([]);
+    for (let row = 0; row < letters.length; row++) {
+      newArray[col].push(letters[row][col]);
+       
     }
-}
-
-module.exports = wordSearch
+     
+  }
+  return newArray;
+};
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return undefined;
+  }
+  if (findWord(letters, word)) {
+    return true;
+  } if (findWord(transpose(letters), word)) {
+    return true;
+  }
+  return false;
+};
+const findWord = (letters, word) => {
+  const joined = letters.map(ls => ls.join(''));
+  for (l of joined) {
+    if (l.includes(word))
+      return true;
+    
+  }
+};
+   
+module.exports = wordSearch;
